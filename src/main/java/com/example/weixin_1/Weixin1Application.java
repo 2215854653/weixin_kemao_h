a93fdacab4b1e538b699edb2dd696a7f26a41065
huangjie <huangjie@DESKTOP-5VAIGHL>
2019-03-31 15:43:53 +0800

    完成接入程序

--- src/main/java/com/example/weixin_1/Weixin1Application.java
+++ src/main/java/com/example/weixin_1/Weixin1Application.java
@@ -3,6 +3,8 @@ package com.example.weixin_1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class Weixin1Application {
 
