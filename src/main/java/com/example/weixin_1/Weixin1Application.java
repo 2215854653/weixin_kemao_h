0d48770cba25d9afe5115606f973eee527870b5c
huangjie <huangjie@DESKTOP-5VAIGHL>
2019-03-31 15:27:46 +0800

    初次提交Spring Boot的项目模板

---
+++ src/main/java/com/example/weixin_1/Weixin1Application.java
@@ -0,0 +1,13 @@
+package com.example.weixin_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Weixin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Weixin1Application.class, args);
+	}
+
+}
